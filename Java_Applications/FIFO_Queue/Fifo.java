0e5362207b54bff2f360c11cfac82b21502a2b3e
Krystian <p3sman@gmail.com>
2015-07-29 15:22:29 +0200

    Queue task

---
+++ Java_Applications/FIFO_Queue/Fifo.java
@@ -0,0 +1,41 @@
+//Usluga sieciowa implementujaca kolejki FIFO
+//dostepna przez http
+//metody dodawania/usuwania kolejek
+//metody dodawania/usuwania elementow z kolejek
+//metoda o stanie kolejek (nazwa, ilosc elementow, szybkosc dodawania/wyciagania elementow z kolejki
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Fifo {
+	public static void main (String[] args) {
+	
+
+	}
+}
+
+class Queue {
+	
+
+	public void addElement {
+	
+	}
+
+	public void deleteElement {
+
+	}
+
+}
+
+class Service {
+
+
+	public void addQueue {
+	
+	}
+
+	public void deleteQueue {
+	queue.remove();
+
+
+	}
